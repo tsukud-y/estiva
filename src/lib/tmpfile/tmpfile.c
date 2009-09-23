3a483aa25db94e258dd7db389e87b0d7d675583e
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2009-09-23 12:34:16 +0900

    tmpfile.c maintenance

--- src/lib/tmpfile/tmpfile.c
+++ src/lib/tmpfile/tmpfile.c
@@ -3,28 +3,32 @@
 
 
 static int tmpcounter = 0;
-static char filename[1000][100];
-static FILE *tmpfp[1000];
+static char filename[100][100];
+static FILE *tmpfp[100];
 
 FILE *estiva_tmpopen(void)
 {
-  sprintf(filename[tmpcounter],"%d",tmpcounter);
-  tmpfp[tmpcounter] = fopen(filename[tmpcounter],"w");
+  int i;
+  for ( i = 0; i<=98; i++) if ( tmpfp[i] == NULL) break;
+  sprintf(filename[i],"%d",tmpcounter);
+  tmpfp[i] = fopen(filename[i],"w");
   tmpcounter++;
-  return tmpfp[tmpcounter -1];
+  return tmpfp[i];
 }
 
 char *estiva_tmpname(FILE *fp)
 {
   int i;
-  for ( i = 1000; i>=0; i--) if ( fp == tmpfp[i]) break;
+  for ( i = 0; i<=98; i++) if ( fp == tmpfp[i]) break;
   return filename[i];
 }
 
 void estiva_tmpclose(FILE *fp)
 {
+  int i;
   char cmd[100];
   sprintf(cmd,"rm %s",tmpname(fp));
   system(cmd);
   fclose(fp);
+  for ( i = 0; i<=98; i++) if ( fp == tmpfp[i]) tmpfp[i] = NULL;
 }
