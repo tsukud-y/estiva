e8559c7db82a2eacf863687afefbf9ce02d09e1b
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2009-10-10 21:52:35 +0900

    -solver bicg

--- src/lib/solver/solver.c
+++ src/lib/solver/solver.c
@@ -3,6 +3,7 @@
 #include <estiva/op.h>
 #include <estiva/solver.h>
 
+extern int estiva_bicgsolver(void *A, double *x, double *b);
 extern int estiva_gausssolver(void *A, double *x, double *b);
 extern int estiva_blusolver(void *A, double *x, double *b);
 extern int estiva_pcgssolver(void *A, double *x, double *b) ;    
@@ -10,6 +11,10 @@ extern int estiva_pcgssolver(void *A, double *x, double *b) ;
 int estiva_solver(void *A, double *x, double *b)
 {
 
+  if (!strcmp(getop("-solver"),"bicg") ){
+    printf("foo\n");
+    return estiva_bicgsolver(A,x,b);
+  }
   if (!strcmp(getop("-solver"),"gauss") ) return estiva_gausssolver(A,x,b);
   if (!strcmp(getop("-solver"),"blu") ) return estiva_blusolver(A,x,b);
   return estiva_pcgssolver(A,x,b);
--- tutorial/10000/main.c
+++ tutorial/10000/main.c
@@ -2,17 +2,21 @@
 #include <estiva/ary.h>
 #include <estiva/mx.h>
 #include <estiva/solver.h>
+#include <estiva/op.h>
 
-main(){
-  int i, n = 10000;
+main(int argc, char **argv){
+  int i, n = 512;
   static MX *A; static double *x, *b;
+  initop(argc,argv);
   initmx(A,n+1,8); ary1(x, n+1); ary1(b, n+1);
 
   for ( i =1; i<=n; i++){
-    mx(A,i,i) = 1.0; b[i] = 1.0;
+    mx(A,i,i) = 2.0; b[i] = 1.0;
   }
+  for (i=1;i<n;i++) mx(A,i,i+1) = -1.0;
+  for (i=1;i<n;i++) mx(A,i+1,i) = -1.0;
+
   solver(A,x,b);
 
-  printf("%f\n", x[1] );
-  printf("%f\n", x[2] );
+  for (i=1;i<=n;i++) printf("%f\n", x[i] );
 }
