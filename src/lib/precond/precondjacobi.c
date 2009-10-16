0b4465393643a55d1e238f38df5972fbcf907601
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2009-10-16 17:42:48 +0900

    -jacobistep avalilable

--- src/lib/precond/precondjacobi.c
+++ src/lib/precond/precondjacobi.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <estiva/ary.h>
 #include <estiva/mx.h>
@@ -8,27 +9,28 @@
 
 void estiva_precondjacobi(MX *A, double *x, double *D, double *b)
 {
-  int i,j,J,k,n;
-  static double *xold;
+  int i,j,J,k,n, step = 2;
+  static double *xo;
+
+  if ( defop("-jacobistep") ) step = atoi(getop("-jacobistep"));
 
   n = dim1(D);
 
-  ary1(xold,n+1);
+  ary1(xo,n+1);
     
-  for (i=0; i<n; i++) xold[i] = 0.0;
+  for (i=0; i<n; i++) xo[i] = 0.0;
 
-  for(k=0;k<3;k++) {
+  for(k=0;k<step;k++) {
     for(i=0;i<n;i++) {
       x[i]=b[i];
       for(j=0; j< A->n; j++) {
 	J = A->IA[i][j];
 	if (J != 0) if ( J-1 != i) if(A->A[i][j] !=0.0) {
-	  x[i] -= A->A[i][j]*xold[J-1];
+	  x[i] -= A->A[i][j]*xo[J-1];
 	}
       }
       x[i]=x[i]*D[i];
     }
-    for(i=0;i<n;i++) xold[i]=x[i];
+    for(i=0;i<n;i++) xo[i]=x[i];
   }
 }
-
