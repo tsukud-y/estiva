d214e28acd29246ced95aed373772b9676938bc4
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2009-10-16 14:18:27 +0900

    precond separate

--- src/lib/solver/bicg.c
+++ src/lib/solver/bicg.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include <estiva/ary.h>
 #include <estiva/mx.h>
 #include <estiva/solver.h>
+#include <estiva/op.h>
 
-static long globalN = 0;
 static MX *A, *AT;
 static double *D;
 
@@ -32,89 +33,73 @@ static int matvectrans(double *alpha, double *x, double *beta, double *y){
 }
 
 
-static int psolve(double *x, double *b){
-  int i,j,J,k, n=globalN;
+void precondnone(long n, double *x, double *b)
+{
+  long i;
+  for (i=0; i<n; i++) x[i] = b[i];
+}
+
+void precondscaling(double *x, double *D, double *b)
+{
+  long i, n;
+  n = dim1(D);
+  for (i=0; i<n; i++) x[i] = D[i]*b[i];
+}
+
+int precondjacobi(MX *A, double *x, double *b)
+{
+  int i,j,J,k,n;
   static double *xold;
 
-  for(i=0; i<globalN; i++) x[i] = D[i]*b[i];
-  return 0;
-  
+  n = dim1(D);
+
   ary1(xold,n+1);
-  
+    
   for (i=0; i<n; i++) xold[i] = 0.0;
 
   for(k=0;k<3;k++) {
     for(i=0;i<n;i++) {
       x[i]=b[i];
-
-#if 0
-      for(j=0;j<n;j++) if(j!=i) if(mx(A,i+1,j+1)!=0.0){
-	x[i] -= mx(A,i+1,j+1)*xold[j];
-	printf(" %d ",j);
-      }
-      printf("\n");
-#endif
-
-
       for(j=0; j< A->n; j++) {
 	J = A->IA[i][j];
 	if (J != 0) if ( J-1 != i) if(A->A[i][j] !=0.0) {
 	  x[i] -= A->A[i][j]*xold[J-1];
-	  //x[i] -= mx(A,i+1,J)*xold[J-1];
-	  //printf("#%d ",J-1);
-	  //t[i-1] += A->A[i-1][j]*x[J-1];
 	}
       }
-      //printf("\n");
-
-
-
-
       x[i]=x[i]*D[i];
     }
     for(i=0;i<n;i++) xold[i]=x[i];
   }
-
-  //printf("c = %d\n",c++);
   return 0;
 }
 
 
-static int psolvetrans(double *x, double *b){
-  int i,j,J,k, n=globalN;
-  static double *xold;
-
-  for(i=0; i<globalN; i++) x[i] = D[i]*b[i];
-  return 0;
-
-  ary1(xold,n+1);
-
-  for (i=0; i<n; i++) xold[i] = 0.0;
-
-  for(k=0;k<3;k++) {
-    for(i=0;i<n;i++) {
-      x[i]=b[i];
-      //for(j=0;j<n;j++) if(j!=i) x[i] -= mx(A,j+1,i+1)*xold[j];
-
-
-      for(j=0; j< AT->n; j++) {
-	J = AT->IA[i][j];
-	if (J != 0) if ( J-1 != i) if(AT->A[i][j] !=0.0) {
-	  x[i] -= AT->A[i][j]*xold[J-1];
-	  //x[i] -= mx(A,i+1,J)*xold[J-1];
-	  //printf("#%d ",J-1);
-	  //t[i-1] += A->A[i-1][j]*x[J-1];
-	}
-      }
-      //printf("\n");
-
-
-
-
-      x[i]=x[i]*D[i];
-    }
-    for(i=0;i<n;i++) xold[i]=x[i];
+static int psolve(double *x, double *b)
+{
+  if (!strcmp(getop("-precond"),"none")) { 
+    precondnone(A->m,x,b); 
+    return 0;
   }
+  if (!strcmp(getop("-precond"),"scaling")) { 
+    precondscaling(x,D,b); 
+    return 0;
+  }
+  precondjacobi(A,x,b);
+  return 0;
+}
+
+
+static int psolvetrans(double *x, double *b)
+{  
+  if (!strcmp(getop("-precond"),"none")) { 
+    precondnone(A->m,x,b); 
+    return 0;
+  }
+  if (!strcmp(getop("-precond"),"scaling")) { 
+    precondscaling(x,D,b); 
+    return 0;
+  }
+  precondjacobi(AT,x,b);
   return 0;
 }
 
@@ -130,7 +115,7 @@ int estiva_bicgsolver(void* pA, double* x, double* b)
 
    printf("phase 2\n");
    n = dim1(b);
-   globalN=n;
+
    ary1(D,n+1);
    for(i=0;i<n;i++) 
      if (mx(A,i+1,i+1) == 0.0) D[i] = 1.0;
