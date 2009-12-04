54aa30b5e532720f10e336b2fe45c5c00738975a
TSUKUDA Yoshio <tsukud-y@chinablue.plala.jp>
2009-12-04 23:43:14 +0900

    psolvemx.c add.

---
+++ src/lib/mx/psolvemx.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <estiva/ary.h>
+#include <estiva/mx.h>
+#include <estiva/precond.h>
+#include <estiva/solver.h>
+#include <estiva/op.h>
+
+void estiva_psolvemx(MX *A, long *pivot, MX *LU, double *D, double *x, double *b)
+{
+  if (!strcmp(getop("-precond"),"none"))
+    precondnone(A->m,x,b); 
+  else if (!strcmp(getop("-precond"),"ILU"))
+    precondILU(pivot,LU,x,b);
+  else if (!strcmp(getop("-precond"),"scaling"))
+    precondscaling(x,D,b); 
+  else 
+    precondjacobi(A,x,D,b);
+}
