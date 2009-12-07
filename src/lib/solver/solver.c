154900681ddb4cf442f661e50ceb003a3f5bca7c
TSUKUDA Yoshio <tsukud-y@chinablue.plala.jp>
2009-12-07 19:43:19 +0900

    CGS solver is add.

--- src/lib/solver/solver.c
+++ src/lib/solver/solver.c
@@ -6,7 +6,9 @@
 extern int estiva_bicgsolver(void *A, double *x, double *b);
 extern int estiva_gausssolver(void *A, double *x, double *b);
 extern int estiva_blusolver(void *A, double *x, double *b);
-extern int estiva_pcgssolver(void *A, double *x, double *b) ;    
+extern int estiva_cgssolver(void *A, double *x, double *b);
+extern int estiva_pcgssolver(void *A, double *x, double *b);    
+
 
 int estiva_solver(void *A, double *x, double *b)
 {
@@ -14,5 +16,6 @@ int estiva_solver(void *A, double *x, double *b)
   if (!strcmp(getop("-solver"),"bicg") )  return estiva_bicgsolver(A,x,b);
   if (!strcmp(getop("-solver"),"gauss") ) return estiva_gausssolver(A,x,b);
   if (!strcmp(getop("-solver"),"blu") )   return estiva_blusolver(A,x,b);
+  if (!strcmp(getop("-solver"),"cgs") )   return estiva_cgssolver(A,x,b);
   return estiva_pcgssolver(A,x,b);
 }
