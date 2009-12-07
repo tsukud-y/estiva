5d6a2bc4cba51b52df49c0af3127e6a652575dc2
TSUKUDA Yoshio <tsukud-y@chinablue.plala.jp>
2009-12-07 20:28:32 +0900

    Bi-CGSTAB solver is add.

--- src/lib/solver/solver.c
+++ src/lib/solver/solver.c
@@ -7,6 +7,7 @@ extern int estiva_bicgsolver(void *A, double *x, double *b);
 extern int estiva_gausssolver(void *A, double *x, double *b);
 extern int estiva_blusolver(void *A, double *x, double *b);
 extern int estiva_cgssolver(void *A, double *x, double *b);
+extern int estiva_bicgstabsolver(void *A, double *x, double *b);
 extern int estiva_pcgssolver(void *A, double *x, double *b);    
 
 
@@ -17,5 +18,6 @@ int estiva_solver(void *A, double *x, double *b)
   if (!strcmp(getop("-solver"),"gauss") ) return estiva_gausssolver(A,x,b);
   if (!strcmp(getop("-solver"),"blu") )   return estiva_blusolver(A,x,b);
   if (!strcmp(getop("-solver"),"cgs") )   return estiva_cgssolver(A,x,b);
+  if (!strcmp(getop("-solver"),"bicgstab"))return estiva_bicgstabsolver(A,x,b);
   return estiva_pcgssolver(A,x,b);
 }
