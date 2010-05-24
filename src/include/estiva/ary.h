db1afcd90c604b4333fd328fc1e072ebd3949b76
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2010-05-25 01:03:02 +0900

    ary1(), ary2() -fno-strict-aliasing

--- src/include/estiva/ary.h
+++ src/include/estiva/ary.h
@@ -2,8 +2,8 @@
 #define _ESTIVA_ARY_H_
 
 
-#define ary1(b,n_1)     estiva_ary1((void **)&b,n_1,sizeof(*b))
-#define ary2(A,m_1,n_1) estiva_ary2((void **)&A,m_1,n_1,sizeof(**A))
+#define ary1(b,n_1)     estiva_ary1((void *)&b,n_1,sizeof(*b))
+#define ary2(A,m_1,n_1) estiva_ary2((void *)&A,m_1,n_1,sizeof(**A))
 #define dim0(v)         estiva_dim0(v)
 #define dim1(v)         estiva_dim1(v)
 #define dim2(v)         estiva_dim2(v)
