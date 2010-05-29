b104b6f3ce16d3de8d367ad8c2925ea274683670
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2010-05-29 11:13:42 +0900

    estiva/que.h

---
+++ src/include/estiva/que.h
@@ -0,0 +1,23 @@
+#ifndef _ESTIVA_QUE_H_
+#define _ESTIVA_QUE_H_
+
+typedef struct {
+  void *elem;
+  void *next;
+} que;
+
+void  estiva_initq(que **q);
+void  estiva_enq(  que  *q, void   *e, size_t n);
+void  estiva_push( que **q, void   *e, size_t n);
+void *estiva_pop(  que **q, void   *e, size_t n);
+void *estiva_lup(  que  *q, void **s2, size_t n);
+int   estiva_forq( que  *q, void   *e, size_t n);
+
+#define initq(q)         estiva_initq(&q)
+#define   enq(q, e)      estiva_enq(   q, (void*)&e, sizeof(e))
+#define  push(q, e)      estiva_push( &q, (void*)&e, sizeof(e))
+#define   pop(q, e)      estiva_pop(  &q, (void*)&e, sizeof(e))
+#define   lup(q, e)      estiva_lup(   q, (void*)&e, sizeof(e))
+#define  forq(q, e) for(;estiva_forq(  q, (void*)&e, sizeof(e));)
+
+#endif
---
+++ src/lib/que/que.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "estiva/que.h"
+
+void estiva_initq(que **q)
+{
+  if ( *q == NULL ) *q = calloc(1,sizeof(que));
+}
+
+
+void estiva_enq(que *q, void *e, size_t n)
+{
+  void **addr;
+  
+  while (1) {
+    if (q->elem == NULL) {
+      addr = q->elem = malloc(sizeof(void *));
+      *addr = malloc(n);
+      memcpy(*addr,e,n);
+      q->next = calloc(1,sizeof(que));
+      return ;
+    }
+    q = q->next;
+  }
+}
+
+
+void estiva_push(que **q, void *e, size_t n)
+{
+  void **addr;
+  que *p;
+
+  p = *q;
+  *q = calloc(1,sizeof(que));
+  addr = (*q)->elem = malloc(sizeof(void *));
+  *addr = malloc(n);
+  memcpy(*addr,e,n);
+  (*q)->next = p;
+}
+
+
+void *estiva_pop(que **q, void *e, size_t n)
+{
+  que *p;
+  p = *q;
+  if (p->elem==NULL) return NULL;
+  memcpy(e,*(void**)p->elem,n);
+  *q = p->next;
+  free(p);
+  return e;
+}
+
+
+void *estiva_lup(que *q, void **s2, size_t n)
+{
+  void **addr, **s1;
+
+  while( q->elem != NULL ) {
+    addr = q->elem;
+    s1 = *addr;
+    if(!strcmp(*s1,*s2)) return s1;
+    q = q->next;
+  }
+  return NULL;
+}
+
+
+
+
+int estiva_forq(que *q, void *e, size_t n)
+{
+  static que *p;
+  
+  if ( p == NULL ) p = q;
+  else p = p->next;
+  if ( p->elem != NULL )  memcpy(e,*(void**)p->elem,n);
+  if ( p->elem == NULL ) { p = NULL; return 0; }
+  return 1;
+}
