1aefd089a4d9f5bb42981bd262b14ff0d4b0de40
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2010-05-29 11:49:29 +0900

    fgetline() renewal

--- src/lib/fgetline/fgetline.c
+++ src/lib/fgetline/fgetline.c
@@ -1,92 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "estiva/op.h"
 #include "estiva/ary.h"
-
-typedef struct {
-  int c;
-  void *next;
-  void *tail;
-} list;
-
-
-static void addlist(list *elem, int c)
-{
-  list *top = elem;
-
-  if ( top->tail == NULL )
-    ;
-  else
-    elem = top->tail;
-
-  while (1) {
-    if ( elem->next == NULL ) {
-      elem->c = c;
-      elem->next = calloc(1,sizeof(list));
-      top->tail = elem;
-      return ;
-    }
-    elem = elem->next;
-  }
-}
-
-
-static unsigned long cntlist(list *top)
-{
-  unsigned long n=0;
-  while (1) {
-    n++;
-    top = top->next;
-    if ( top == NULL ) return n;
-  }
-}
-
-
-static char *list2str(list *top, char *str)
-{
-  unsigned long i=0;
-  for ( ;top->next != NULL;i++) {
-    str[i] = top->c;
-    top = top->next;
-  }
-  str[i] = 0;
-  return str;
-}
-
-
-static void rmlist(list *p)
-{
-  list *tmp;
-  for ( p = p->next; p; p = tmp ) {
-    tmp = p->next;
-    free(p);
-  }
-}
+#include "estiva/que.h"
 
 
 char *estiva_fgetline(FILE *fp)
 {
-  static list *top;
-  static char *buf, *append;
-  unsigned long i, j, n=1000;
+  static char *buf, *ret;
+  static que *q;
+  unsigned long i, j, n=80;
   int c; 
 
   ary1(buf,n+1);
-
-  ary1(top,1);
-  rmlist(top);
-  top->next = NULL;
-  top->tail = NULL;
-
-  for ( i=0, j=0; EOF != (c = fgetc(fp)) && c != '\n'; ){
-    if ( i < n )
-      buf[i++] = c;
-    else 
-      addlist(top,c);
-  }
+  initq(q); while ( NULL != pop(q,c) );
+  
+  for ( i=0; EOF != (c = fgetc(fp)) && c != '\n'; )
+    if ( i < n )  buf[i++] = c;
+    else          enq(q,c);
   buf[i] = 0;
-
-  ary1(append, cntlist(top)+1);
-  return strcat(buf,list2str(top,append));
+  
+  j = 0; forq(q,c) j++;
+  
+  ary1(ret,n+j+2);
+  ret[0] = 0;
+  strcat(ret,buf);
+  
+  forq(q,c) ret[i++] = c;
+  return ret;
 }
