ab892b24f2aec5758e18dd7eeae4ba5f6ee34031
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2010-06-03 22:25:45 +0900

    cp() is enhanced.

--- src/lib/std/std.c
+++ src/lib/std/std.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "estiva/std.h"
+#include "estiva/ary.h"
 
 void estiva_swap(void *A, void *B, int size1, int size2)
-{ static char p3[128], *p1, *p2;static int i;
+{ static char *p3, *p1, *p2;static int i;
   p1=(char *)A;
   p2=(char *)B;
+
+  ary1(p3,size1);
   
-  if(size1 == size2 && size1 <= 128) 
+  if(size1 == size2 ) {
     forall(0,i,size1-1){ p3[i]=p2[i]; p2[i]=p1[i]; p1[i]=p3[i];}
+  }
   else{ fprintf(stderr,"estiva_swap: size mismatch\n"); exit(1);}
 }
 
@@ -21,14 +25,3 @@ void estiva_cp(void *A, void *B, int size1, int size2)
   else{ fprintf(stderr,"estiva_cp: size mismatch\n"); exit(1);}
 }
 
-#if 0
-static int flag;
-void estiva_initecho(void)
-{flag=1;}
-
-int estiva_echo(char *str)
-{ 
-  if(flag){ fprintf(stderr,"%s",str);flag=0;return 1;}
-  return 0;
-}
-#endif
