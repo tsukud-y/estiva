aa9cfff4c2f5fc2baeeeade71076f625a0d576f1
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2010-06-10 21:13:52 +0900

    pltmx()

--- src/include/estiva/mx.h
+++ src/include/estiva/mx.h
@@ -21,6 +21,8 @@ typedef struct {
 #define mx(A,i,j)           (*estiva_mx(A,i,j))
 #define matvecmx(A,alpha,x,beta,y) estiva_matvecmx(A,alpha,x,beta,y)
 #define psolvemx(A,pivot,LU,D,x,b) estiva_psolvemx(A,pivot,LU,D,x,b)
+#define pltmx(A)                   estiva_pltmx(A)
+
 
 extern void    estiva_initmx(MX **Ap, long i, long j);
 extern void    estiva_transmx(MX **ATp, MX *A);
@@ -29,5 +31,6 @@ extern void    estiva_mulmx(double **tp, MX *A, double *x);
 extern double *estiva_mx(MX *T, long i, long j);
 extern void    estiva_matvecmx(MX *A, double *alpha, double *x, double *beta, double *y);
 extern void    estiva_psolvemx(MX *A, CRS *pivot, MX *LU, double *D, double *x, double *b);
+extern void    estiva_pltmx(MX *A);
 
 #endif
