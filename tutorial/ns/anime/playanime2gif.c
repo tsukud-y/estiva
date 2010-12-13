f76702e50eff80377b3f9792105cda4e87e3be8a
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2010-12-13 21:43:18 +0900

    playanime2gif command is created.

---
+++ tutorial/ns/anime/playanime2gif.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char *num2str(long n){
+  long i;
+  static char ret[100];
+  sprintf(ret,"00000000%ld",n);
+  i = strlen( ret );
+  
+  return &ret[i-8];
+}
+
+
+int main(int argc, char **argv) {
+  FILE *pp, *fp;
+  int c;  
+  static long sequence=0;
+  static char filename[400], cmd[400];
+
+  if (argc == 2 ) sequence = atoi(argv[1]);
+
+  pp = popen("gnuplot","w");
+  
+  while (1) {
+    printf("sequencde = %ld\n",sequence);
+    sprintf(filename,"%ld.gnuplot",sequence++);
+    fp = fopen(filename,"r");
+    if ( NULL == fp ) return 0;
+
+    fprintf(pp,"set terminal gif\n");
+    sprintf(cmd,"set output \"%s.gif\"\n",num2str(sequence));
+    fprintf(pp,"%s",cmd);
+    while ( EOF != (c = fgetc(fp)) ){
+      fprintf(pp,"%c",c);
+    }
+    fclose(fp);
+    fflush(pp);
+    usleep(100);
+  }
+  return 0;
+}
