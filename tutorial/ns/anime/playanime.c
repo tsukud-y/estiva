c0394da4496dd77ce4029ec6f9a882898b3c31e4
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2010-12-13 12:00:00 +0900

    playanime %d,  %d is start number.

--- tutorial/ns/anime/playanime.c
+++ tutorial/ns/anime/playanime.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char **argv) {
   FILE *pp, *fp;
   int c;  
   static long sequence=0;
   static char filename[400];
 
+  if (argc == 2 ) sequence = atoi(argv[1]);
+
   pp = popen("gnuplot","w");
   
   while (1) {
-    static long k = 0;
+    printf("sequencde = %ld\n",sequence);
     sprintf(filename,"%ld.gnuplot",sequence++);
     fp = fopen(filename,"r");
     if ( NULL == fp ) return 0;
@@ -20,8 +24,6 @@ int main() {
     }
     fclose(fp);
     fflush(pp);
-    printf("k = %ld\n",k++);
-    fflush(stdout);
     usleep(100);
   }
   return 0;
