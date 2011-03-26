da03bf7f16d05153c2397d1f051015011c31e3dc
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2011-03-26 17:09:44 +0900

    boundary condition "slip" -> "sticking"

--- tutorial/ns/boundary_condition.c
+++ tutorial/ns/boundary_condition.c
@@ -3,7 +3,7 @@
 
 void estiva_boundary_condition(MX *A, double *b)
 {
-  static double velocity = 100.0;
+  static double velocity = 1000.0;
   long i, NUM, m, n;
   static xyc *Z; static nde *N; static double *S;
 
@@ -21,21 +21,30 @@ void estiva_boundary_condition(MX *A, double *b)
 
 
   forgammap2(i,"south",Z,N) {
+    zerofillrow(A,i);
     zerofillrow(A,i+m);
+    mx(A,i,i)     = 1.0;
     mx(A,i+m,i+m) = 1.0;
+    b[i]          = 0.0;
     b[i+m]        = 0.0;
   }
 
   forgammap2(i,"east",Z,N) {
     zerofillrow(A,i);
-    mx(A,i,i) = 1.0;
-    b[i]      = 0.0;
+    zerofillrow(A,i+m);
+    mx(A,i,i)     = 1.0;
+    mx(A,i+m,i+m) = 1.0;
+    b[i]          = 0.0;
+    b[i+m]        = 0.0;
   }
 
   forgammap2(i,"west",Z,N) {
     zerofillrow(A,i);
-    mx(A,i,i) = 1.0;
-    b[i]      = 0.0;
+    zerofillrow(A,i+m);
+    mx(A,i,i)     = 1.0;
+    mx(A,i+m,i+m) = 1.0;
+    b[i]          = 0.0;
+    b[i+m]        = 0.0;
   }
 
   if ( defop("-velocity") ) velocity = atof(getop("-velocity"));
