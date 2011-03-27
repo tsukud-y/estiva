7879cc2c36b754391737923affb6116ecd1dd67d
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2011-03-28 02:49:11 +0900

    foreach.c is arranged.

--- src/lib/foreach/foreach.c
+++ src/lib/foreach/foreach.c
@@ -1,75 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <estiva/foreach.h>
+#include <string.h>
+#include "estiva/foreach.h"
+#include "estiva/std.h"
+#include "estiva/ary.h"
 
-#define  forall(m,i,n) for(i=m;i<=n;i++) 
+static int top, limit = 1;
+static void **x_array, **y_array;
 
-
-static void estiva_cp(void *A, void *B, int size1, int size2)
-     /* cp(a,b)     estiva_cp(&(a),&(b),sizeof(a),sizeof(b)) */
-{ static int i; char *p1, *p2;
-  p1=(char *)A;
-  p2=(char *)B;
-
-  if(size1 == size2) forall(0,i,size1-1) p2[i]=p1[i]; 
-  else{ fprintf(stderr,"estiva_cp: size mismatch\n"); abort();}
+static void set(int i, void *x, void *y)
+{
+  x_array[i] = x; y_array[i] = y;
 }
 
-
-
-#define  R(x,y) estiva_R(x,y) 
-#define  f(x)   estiva_f(x) 
-
-#define set(i,xi,yi) (x[i]=(xi),y[i]=(yi))
-
-static void *x[20],*y[20];
-static int tail=0;
-
-static void *X(int n)
-{ return x[n];}
-
-static void *Y(int n)
-{ return y[n];}
-
-static int n(void *xi)
-{ int i=0; forall(0,i,tail)if(X(i)==xi) break; return i;}
-
-static void estiva_R(void *xi, void *yi)
-     /* R(x,y) estiva_R(x,y) */
+static int where(void *x)
 { 
-  if(yi==NULL)
-    { int i;i=n(xi);set(i,NULL,NULL); if(i==tail) tail--;}
+  int i; 
+  forall(0,i,top) if( x_array[i] == x ) break; 
+  return i;
+}
+
+static void R(void *x, void *y)
+{ 
+  int i;
+
+  if ( y == NULL ) { 
+    i = where(x);
+    set(i,NULL,NULL); 
+    if ( i == top ) top--;
+  }
   else{
-    R(xi,NULL);
-    { int i;i=n(NULL);set(i,xi,yi); if(i==tail) tail++;}
+    R(x,NULL);
+    i = where(NULL);
+    set(i,x,y); 
+    if ( i == top ) {
+      top++;
+      if ( top >= limit ) {
+	static void **tx, **ty;
+	
+	ary1(tx,limit); ary1(ty,limit);
+	
+	forall(0,i,top) tx[i] = x_array[i];
+	forall(0,i,top) ty[i] = y_array[i];
+
+	limit++;
+	
+	ary1(x_array,limit); ary1(y_array,limit);
+
+	forall(0,i,top) x_array[i] = tx[i];
+	forall(0,i,top) y_array[i] = ty[i];
+      }
+    }
   }
 }
 
-static void *estiva_f(void *xi)
-     /* f(x)   estiva_f(x) */
-{ return Y(n(xi));}
+static void *f(void *x)
+{ 
+  ary1(x_array,limit); ary1(y_array,limit);
+  return y_array[where(x)];
+}
 
+#define y(x) (*(int *)f(x))
 
-#define n(f0) (*(int *)f(f0))
+static void new(void *x)
+{
+  R(x,malloc(sizeof(int)));
+  y(x) = 0;
+}
 
-int estiva_foreach(int fsize,void *f0,...)
-     /* foreach(f) while(estiva_foreach(sizeof(f),&f, */
-{ int i; void *fi, *fn; va_list ap;
+static void destroy(void *x)
+{
+  free(&y(x));
+  R(x, NULL);
+}
+
+int estiva_foreach(int xsize, void *x, ...)
+{ 
+  va_list ap;
+  void *xi, *xn;
+  int i; 
   
-  if(fsize==0||f0==NULL) return 0;    
-  if(f(f0)==NULL){ R(f0,malloc(sizeof(int))); n(f0) = 0;}
-  if(f(f0)==NULL) abort();
+  if ( xsize == 0    ) return 0;
+  if ( x     == NULL ) return 0;    
+  if ( &y(x) == NULL ) new(x);
+  if ( &y(x) == NULL ) abort();
   
-  va_start(ap,f0); fi = f0; 
-  forall(1,i,n(f0)) fi = va_arg(ap, void *);
-  fn = va_arg(ap,void *);
+
+  va_start(ap,x); 
+  xi = x;
+  forall(1,i,y(x)) 
+    xi = va_arg(ap, void *);
+  xn = va_arg(ap,void *);
   va_end(ap); 
   
-  estiva_cp(f0,fi,fsize,fsize);
-  if(fn!=NULL){ estiva_cp(fn,f0,fsize,fsize); n(f0)++; return 1;}
-  free(f(f0)); R(f0,NULL); return 0;
+  memcpy(xi,x,xsize);
+
+
+  if ( xn != NULL ) { 
+    memcpy(x,xn,xsize);
+    y(x)++; 
+    return 1;
+  }
+  destroy(x);
+  return 0;
 }
+
 void *estiva_foreachend()
-     /* end estiva_end())) */
-{ return NULL;}
+{ 
+  return NULL;
+}
