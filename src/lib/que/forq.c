4b71a5e3646bd9f2994b189b9f5ecefd8e6a0e12
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2011-03-28 04:06:04 +0900

    forq.c is arranged.

--- src/lib/que/forq.c
+++ src/lib/que/forq.c
@@ -1,65 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "estiva/que.h"
+#include "estiva/std.h"
 
+#define y(x) (*(que**)f(x))
 
-#define  forall(m,i,n) for(i=m;i<=n;i++) 
-#define  R(x,y) estiva_R(x,y) 
-#define  f(x)   estiva_f(x) 
-
-#define set(i,xi,yi) (x[i]=(xi),y[i]=(yi))
-
-static void *x[20],*y[20];
-static int tail=0;
-
-static void *X(int n)
-{ return x[n];}
-
-static void *Y(int n)
-{ return y[n];}
-
-static int n(void *xi)
-{ int i=0; forall(0,i,tail)if(X(i)==xi) break; return i;}
-
-static void estiva_R(void *xi, void *yi)
-/* R(x,y) estiva_R(x,y) */
-{ 
-  if(yi==NULL)
-    { int i;i=n(xi);set(i,NULL,NULL); if(i==tail) tail--;}
-  else{
-    R(xi,NULL);
-    { int i;i=n(NULL);set(i,xi,yi); if(i==tail) tail++;}
-  }
+void estiva_forq(que *q, void **x)
+{
+  if (&y(x) == NULL) Rnew(x,void*);
+  if (&y(x) == NULL) abort();
+  y(x) = q;
 }
 
-static void *estiva_f(void *xi)
-/* f(x)   estiva_f(x) */
-{ return Y(n(xi));}
-
-
-static que **estiva_p(void **e)
+int estiva_forq_loop(void **x)
 {
-  return  f(e);
-}
-
-
-#define p(e)  (*estiva_p(e))
-
-void estiva_forq(que *q, void **e)
-{
-  if (f(e) == NULL) R(e,malloc(sizeof(void *)));
-  p(e) = q;
-}
-
-int estiva_forq_loop(void **e)
-{
-  if ( p(e)->elem == NULL ) { 
-    free(f(e)); 
-    R(e,NULL);
+  if ( y(x)->elem == NULL ) { 
+    Rdestroy(x);
     return 0; 
   }
-  *e = *(void**)p(e)->elem;
-  p(e) = p(e)->next;
+  *x = *(void**)y(x)->elem;
+  y(x) = y(x)->next;
   return 1;
 }
