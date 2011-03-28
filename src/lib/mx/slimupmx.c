bfa225dbdc42d48024d076e4ba05390770d866fb
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2011-03-29 08:30:50 +0900

    slimupmx() is add.

---
+++ src/lib/mx/slimupmx.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "estiva/std.h"
+#include "estiva/ary.h"
+#include "estiva/mx.h"
+#include "estiva/solver.h"
+
+void estiva_slimupmx(MX **M, MX *A)
+{
+  long i, j, J,  maxw=0, ii=0, jj=0, w=0;  
+
+  mx(A,1,1) = mx(A,1,1);
+
+  for (i = ii; i < A->m; i++, ii++, jj=0, w=0) 
+    for (j = jj; j< A->n; j++) 
+      if (A->IA[i][j] != 0.0) {
+	maxw = max(maxw,w++);
+        jj = j+1;
+      }
+
+  initmx((*M),A->m+1,maxw);
+
+  for(i=1;i<= A->m;i++) for(j=0; j< A->n; j++) {
+      J = A->IA[i-1][j];
+      if (J != 0) mx((*M),i,J) = A->A[i-1][j];
+    }
+}
+
