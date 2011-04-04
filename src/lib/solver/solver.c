1e836a6e7cf85dab01a5bf07453d9a2b7f80c4a8
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2011-04-05 07:44:28 +0900

    QMR solver is deployed.

--- src/lib/solver/solver.c
+++ src/lib/solver/solver.c
@@ -3,21 +3,22 @@
 #include <estiva/op.h>
 #include <estiva/solver.h>
 
-extern int estiva_bicgsolver(void *A, double *x, double *b);
-extern int estiva_gausssolver(void *A, double *x, double *b);
-extern int estiva_blusolver(void *A, double *x, double *b);
-extern int estiva_cgssolver(void *A, double *x, double *b);
-extern int estiva_bicgstabsolver(void *A, double *x, double *b);
-extern int estiva_pcgssolver(void *A, double *x, double *b);    
-
+int estiva_bicgsolver(void *A, double *x, double *b);
+int estiva_gausssolver(void *A, double *x, double *b);
+int estiva_blusolver(void *A, double *x, double *b);
+int estiva_cgssolver(void *A, double *x, double *b);
+int estiva_bicgstabsolver(void *A, double *x, double *b);
+int estiva_pcgssolver(void *A, double *x, double *b);    
+int estiva_qmrsolver(void *A, double *x, double *b);
 
 int estiva_solver(void *A, double *x, double *b)
 {
-
+  if (!strcmp(getop("-solver"),"pcgs") )  return estiva_pcgssolver(A,x,b);
   if (!strcmp(getop("-solver"),"bicg") )  return estiva_bicgsolver(A,x,b);
   if (!strcmp(getop("-solver"),"gauss") ) return estiva_gausssolver(A,x,b);
   if (!strcmp(getop("-solver"),"blu") )   return estiva_blusolver(A,x,b);
   if (!strcmp(getop("-solver"),"cgs") )   return estiva_cgssolver(A,x,b);
   if (!strcmp(getop("-solver"),"bicgstab"))return estiva_bicgstabsolver(A,x,b);
-  return estiva_pcgssolver(A,x,b);
+  if (!strcmp(getop("-solver"),"qmr"))     return estiva_qmrsolver(A,x,b);
+  return estiva_qmrsolver(A,x,b);
 }
