c9328a0037c58d798f8537841dc6050c64930b87
TSUKUDA Yoshio <tsukud-y@galaxy.ocn.ne.jp>
2011-08-02 23:13:30 +0900

    I like C.

--- tutorial/ViennaCLpsc98/main.cpp
+++ tutorial/ViennaCLpsc98/main.cpp
@@ -1,11 +1,8 @@
-//#include <stdio.h>
-//#include "estiva/ary.h"
-//#include "estiva/mx.h"
-//#include "estiva/op.h"
-//#include "estiva/solver.h"
-//#include "estiva/std.h"
-//#include "estiva/vec.h"
-//#include "estiva/eblas.h"
+#include <stdio.h>
+
+/*#include "viennacl.h"                                 */
+#ifndef _VIENNACL_H_
+#define _VIENNACL_H_
 #include <iostream>
 #include <viennacl/scalar.hpp>
 #include <viennacl/vector.hpp>
@@ -16,93 +13,80 @@
 #include <viennacl/linalg/row_scaling.hpp>
 #include <viennacl/linalg/jacobi_precond.hpp>
 
+using viennacl::linalg::ilut_precond;
+using viennacl::linalg::jacobi_precond;
+using viennacl::linalg::row_scaling;
+using viennacl::compressed_matrix;
+using viennacl::linalg::cg_tag;
+using viennacl::linalg::solve;
+using viennacl::linalg::ilut_tag;
+using viennacl::linalg::jacobi_tag;
+using viennacl::linalg::row_scaling_tag;
 
+typedef compressed_matrix<double>  SparseMatrix;
+typedef std::vector< std::map< unsigned int, double> > matrix;
+typedef std::vector<double> vector;
+typedef viennacl::compressed_matrix<double> gpumatrix;
+typedef viennacl::vector<double> gpuvector;
+typedef ilut_precond< SparseMatrix >  ILU;
+typedef row_scaling< SparseMatrix > Scaling;
+typedef jacobi_precond< SparseMatrix > Jacobi;
 
 extern "C" {
-int genmat(int,int*,double*,double*);
-int chkval(FILE*,int,double*);
+  int genmat(int,int*,double*,double*);
+  int chkval(FILE*,int,double*);
 }
 
+vector gpusolver( matrix A, vector b)
+{
+  int n = A.size();
+  vector x(n);
+  gpumatrix Agpu(n,1); gpuvector bgpu(n), xgpu(n);
+
+  copy(A, Agpu); copy(b.begin(), b.end(), bgpu.begin());
+
+  ILU     vcl_ilut(Agpu, ilut_tag(8,1e-3));
+  Scaling vcl_row_scaling(Agpu, row_scaling_tag(2));
+  Jacobi  vcl_jacobi(Agpu,jacobi_tag());
+  cg_tag  custom_cg(1e-7,1000000);
+
+  xgpu = solve(Agpu, bgpu, custom_cg, vcl_jacobi);
+
+  copy(xgpu.begin(), xgpu.end(), x.begin());
+  return x;
+}
+#endif
+/***********************************************************/
+
+
 int main(int argc, char **argv){
-  //static MX *A;
-  static int JA[10];
-  static double AA[10], b[2000001], x[2000001];
+  vector AA(10);
   double B;
-  int    i, j, n, w;
+  int    JA[10], i, j, n, w;
 
-  //initop(argc,argv);
-
-
-  genmat(-1,JA,AA,&B);
+  genmat(-1,&JA[0],&AA[0],&B);
   n = JA[0]; w = JA[2];
-  std::vector< std::map< unsigned int, double> > cpu_sparse_matrix(n);
-  std::vector<double> stl_vec(n);
 
-
-  //initmx(A,n+1,w+1);  ary1(x,n+1);  ary1(b,n+1);
+  matrix A(n); vector x(n), b(n);
 
   for ( i=1; i<=n; i++) {
     for (j=0;j<=w-1;j++) {
       JA[j] =  -1;
       AA[j] = 0.0;
     }
-    genmat(i,JA,AA,&B);
+    genmat(i,&JA[0],&AA[0],&B);
     for ( j=0; j<w; j++) if (JA[j] != -1){
 	if( JA[j] <=0 || n < JA[j] ) {
 	  ;
 	} else {
-	  //mx(A,i,JA[j]) = AA[j]; 
-	  cpu_sparse_matrix[i-1][JA[j]-1] = AA[j];
-	  stl_vec[i-1] = b[i] = B;
+	  A[i-1][JA[j]-1] = AA[j];
+	  b[i-1] = B;
 	}
       }
   }
-  printf("Set up finished.\n");
-  viennacl::compressed_matrix<double> vcl_sparse_matrix(n,1);
-  viennacl::vector<double> vcl_vec(n);
-  viennacl::vector<double> vcl_result(n);
-  copy(cpu_sparse_matrix, vcl_sparse_matrix);
-  copy(stl_vec.begin(), stl_vec.end(), vcl_vec.begin());
 
-
-  viennacl::linalg::cg_tag custom_cg(1e-7,100000);
-
-
-
-  using viennacl::compressed_matrix;
-
-  typedef compressed_matrix<double>  SparseMatrix;
+  x = gpusolver(A,b);
   
-  //ilut_precond<SparseMatrix> vcl_ilut(vcl_sparse_matrix, viennacl::linalg::ilut_tag());
-
-
-  using viennacl::linalg::ilut_precond;
-  using viennacl::linalg::jacobi_precond;
-  using viennacl::linalg::row_scaling;
-
-#if 0
-  ilut_precond< SparseMatrix > vcl_ilut(vcl_sparse_matrix,
-					viennacl::linalg::ilut_tag(8,1e-3));
-
-
-  row_scaling< SparseMatrix > vcl_row_scaling(vcl_sparse_matrix,
-					      viennacl::linalg::row_scaling_tag(2));
-#endif
-
-  jacobi_precond< SparseMatrix > vcl_jacobi(vcl_sparse_matrix,
-					    viennacl::linalg::jacobi_tag());
-
-
-  vcl_result = viennacl::linalg::solve(vcl_sparse_matrix,vcl_vec,
-				       custom_cg, vcl_jacobi);
-
-
-  copy(vcl_result.begin(), vcl_result.end(), stl_vec.begin());
-
-  for ( i = 0; i<n; i++) x[i] = stl_vec[i];
-
   chkval(stdout,n,&x[0]);
-
-
   return 0;
 }
